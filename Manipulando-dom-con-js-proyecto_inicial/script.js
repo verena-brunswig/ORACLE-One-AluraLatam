a7d69120461a606d9add1f0bea2ef9f73f190e1e
Verena Brunswig <verena.brunswig@gmail.com>
2023-08-03 18:25:36 -0300

    orden modulos y finalizacion primer etapa

--- Manipulando-dom-con-js-proyecto_inicial/script.js
+++ Manipulando-dom-con-js-proyecto_inicial/script.js
@@ -1,4 +1,7 @@
-(() => {
+
+    import checkComplete from "./components/checkComplete.js";
+    import deleteIcon from "./components/deleteIcon.js";
+    
     const btn = document.querySelector('[data-form-btn]');
   
     const createTask = (evento) => {
@@ -11,34 +14,17 @@
       input.value = '';
       //backticks
       const taskContent = document.createElement('div');
-  
       const titleTask = document.createElement('span');
       titleTask.classList.add('task');
       titleTask.innerText = value;
       taskContent.appendChild(checkComplete());
       taskContent.appendChild(titleTask);
-  
-      const content = `
-      <i class="fas fa-trash-alt trashIcon icon"></i>`;
       // task.innerHTML = content;
       task.appendChild(taskContent);
+      task.appendChild(deleteIcon());
       list.appendChild(task);
     };
   
     //Arrow functions o funciones anonimas
     btn.addEventListener('click', createTask);
   
-    const checkComplete = () => {
-      const i = document.createElement('i');
-      i.classList.add('far', 'fa-check-square', 'icon');
-      i.addEventListener('click', completeTask);
-      return i;
-    };
-    // Immediately invoked function expression IIFE
-    const completeTask = (event) => {
-      const element = event.target;
-      element.classList.toggle('fas');
-      element.classList.toggle('completeIcon');
-      element.classList.toggle('far');
-    };
-  })();
